8f6d9e3ea0f2a6b524313c1cdca953685d195ce8
kinahiroki <62067896+kinahiroki@users.noreply.github.com>
2021-07-19 19:45:31 +0900

    電卓アプリ更新
    メッセージ定数クラス　新規追加
    チェックUtilityクラス　改行コードの変更

---
+++ CalculatorApp/Common/Constants/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorApp.Common.Constants
+{
+    /// <summary>
+    /// メッセージ定数クラス
+    /// </summary>
+    /// <remarks>
+    /// メッセージ定数クラス
+    /// </remarks>
+    public class Message
+    {
+        /// <summary>アプリ起動メッセージ</summary>
+        /// <remarks>アプリ起動メッセージ</remarks>
+        public static readonly string MSG_INFO_PROCESS_START = "電卓アプリ　起動";
+
+        /// <summary>アプリ終了メッセージ</summary>
+        /// <remarks>アプリ終了メッセージ</remarks>
+        public static readonly string MSG_INFO_PROCESS_END = "電卓アプロ　終了";
+
+        /// <summary>アプリ操作_入力依頼メッセージ</summary>
+        /// <remarks>{0}には入力してほしい項目名を置換する。</remarks>
+        public static readonly string MSG_INFO_APP_OPERATION_INPUT_VALUE = "{0}を入力してください。";
+    }
+}
