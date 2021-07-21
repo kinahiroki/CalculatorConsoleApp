72d1d683ac429a4ba073356b38d50fa773b04dfe
kinahiroki <62067896+kinahiroki@users.noreply.github.com>
2021-07-22 00:04:45 +0900

    電卓アプリ　更新
    足し算ロジック　不要なテストケース削除

--- CalculatorAppTests/Logic/AdditionLogicTest.cs
+++ CalculatorAppTests/Logic/AdditionLogicTest.cs
@@ -16,28 +16,6 @@ namespace CalculatorAppTests.Logic
         /// 足し算処理　テストケース
         /// </summary>
         /// <remarks>
-        /// 以下のテストケースを実施すること。
-        /// ①整数同士の足し算
-        /// </remarks>
-        [Test]
-        public void AddTest()
-        {
-            // Arrange テスト前の準備
-            var number1 = "1000";
-            var number2 = "2000";
-
-            // Act テスト対象の呼び出し
-            var testTarget = new AdditionLogic();
-            var actualResult = testTarget.Add(number1, number2);
-
-            // Assert 実施結果が期待結果通りになっているか？
-            Assert.AreEqual("3000", actualResult);
-        }
-
-        /// <summary>
-        /// 足し算処理　テストケース
-        /// </summary>
-        /// <remarks>
         /// 以下のテストケース実施
         /// ①整数同士の足し算
         /// ②小数同士の足し算
@@ -47,7 +25,7 @@ namespace CalculatorAppTests.Logic
         /// <param name="expetedResult">期待結果</param>
         [TestCase("100", "100", "200")]
         [TestCase("100.1", "100.1", "200.2")]
-        public void AddTest002(string num1, string num2, string expetedResult)
+        public void AddTest(string num1, string num2, string expetedResult)
         {
             // Act テスト対象呼び出し
             var testTarget = new AdditionLogic();
